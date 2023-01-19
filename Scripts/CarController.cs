9be40749e29c20ad163c396c308bcba6b2fa96e6
jeptun <jeptun@gmail.com>
2023-01-19 22:05:54 +0100

    Car input test

---
+++ Scripts/CarController.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CarController : MonoBehaviour
+
+{
+    //public float speed;
+    //private Vector2 move;
+
+    //public void OnMove(InputAction.CallbackContext context)
+    //{
+    //    move= context.ReadValue<Vector2>();
+    //}
+
+    //private void Update()
+    //{
+    //    movePlayer();
+    //}
+
+    //private void movePlayer()
+    //{
+    //        Vector3 movement = new Vector3(move.x, 0f, move.y);
+
+    //        transform.Translate(movement * speed * Time.deltaTime, Space.World);
+    //}
+
+    [Header("Movement Params")]
+    [SerializeField] private float runSpeed = 6.0f;
+
+    private BoxCollider box;
+    private Rigidbody rb;
+
+    /*
+     * parametr pro input movement
+     */
+    Vector2 moveDirection = Vector2.zero;
+
+    private Vector2 move;
+    private void Awake()
+    {
+        box = GetComponent<BoxCollider>();
+        rb = GetComponent<Rigidbody>();
+        //  move= GetComponent<Vector2>();
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        HandleInput();
+        HandleMovement();
+    }
+    private void HandleInput()
+    {
+        moveDirection = InputManager.instance.GetMoveDirection();
+    }
+    public void HandleMovement()
+    {
+        moveDirection = InputManager.instance.GetMoveDirection();
+        Vector3 vector3 = new Vector3(moveDirection.x, 0f, moveDirection.y);
+
+        transform.Translate(vector3 * runSpeed * Time.deltaTime, Space.World);
+
+        // rb.velocity = new Vector2(moveDirection.x * runSpeed, rb.velocity.y);
+        // rb.AddForce(new Vector3(moveDirection.x, 0, moveDirection.y) * runSpeed, ForceMode.Force);
+    }
+
+}
---
+++ Scripts/InputManager.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(PlayerInput))]
+public class InputManager : MonoBehaviour
+{
+    private Vector2 moveDirection;
+
+
+    public static InputManager instance { get; private set; }
+
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Debug.LogError("Found more than one Input Manager in the scene.");
+        }
+        instance = this;
+    }
+
+    public void MovedPresed(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            moveDirection = context.ReadValue<Vector2>();
+        }
+
+        else if (context.canceled)
+        {
+            moveDirection = context.ReadValue<Vector2>();
+        }
+    }
+
+    public Vector2 GetMoveDirection()
+    {
+        return moveDirection;
+    }
+
+}
+
+//private Rigidbody rbody;
+//private PlayerInput playerInput;
+//private PlayerInputActions playerInputActions;
+
+
+//private void Awake()
+//{
+//    rbody = GetComponent<Rigidbody>();
+//    playerInput = GetComponent<PlayerInput>();
+
+//    //    playerInputActions = new PlayerInputActions();
+//    //    playerInputActions.Player.Enable();
+//}
+
+// Update is called once per frame
+//void FixedUpdate()
+//{
+//    MovedForw();
+
+//}
+
+//public void MovePressed(InputAction.CallbackContext context)
+//{
+//    Vector2 inputVector = context.ReadValue<Vector2>();
+//    float speed = 5f;
+//    rbody.AddForce(new Vector3(inputVector.x, 0, inputVector.y) * speed, ForceMode.Force);
+//}
+
+//private void MovedForw()
+//{
+//    rbody.velocity = new Vector3(0, 1, 0);
+//}
+
